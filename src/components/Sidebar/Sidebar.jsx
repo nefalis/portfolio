d3c454ba972e2601bde63e5a115677c29929ebd1
nefalis <nefalis@gmail.com>
2025-10-15 14:50:01 +0200

    delete pew

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -6,10 +6,8 @@ import ImgSidebar from '../../assets/sidebarImg.png';
 import ToggleButton from '../../components/toggleBtn/ToggleBtn';
 import { SiAiqfome, SiChainguard } from "react-icons/si";
 import { GiRoundBottomFlask } from "react-icons/gi";
-import pew from '../../assets/pew.mp3';
 import './sidebar.css';
 
-const audio = new Audio(pew);
 
 const Sidebar = ({ isOn, setIsOn }) => {
     return (
@@ -27,19 +25,19 @@ const Sidebar = ({ isOn, setIsOn }) => {
 
                 <ul className="sidebar flex flex-col space-y-6 w-full">
                     <li className="sidebarItem bg-[#f998cf] rounded-lg w-2/3 ml-15">
-                        <NavLink to='/home' className="sidebarLink flex items-center gap-2" onClick={() => audio.play()}>
+                        <NavLink to='/home' className="sidebarLink flex items-center gap-2">
                             <SiAiqfome className="icon" /> Accueil
                         </NavLink>
                     </li>
 
                     <li className="sidebarItem bg-[#f5f994] rounded-lg w-2/3 ml-20">
-                        <NavLink to='/about' className="sidebarLink flex items-center gap-2" onClick={() => audio.play()}>
+                        <NavLink to='/about' className="sidebarLink flex items-center gap-2">
                             <SiChainguard className="icon" /> Présentation
                         </NavLink>
                     </li>
 
                     <li className="sidebarItem bg-[#7ef8c7] rounded-lg w-2/3 ml-15">
-                        <NavLink to='/project' className="sidebarLink flex items-center gap-2" onClick={() => audio.play()}>
+                        <NavLink to='/project' className="sidebarLink flex items-center gap-2">
                             <GiRoundBottomFlask className="icon" /> Projets
                         </NavLink>
                     </li>
